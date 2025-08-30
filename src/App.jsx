5ee07e50ec612866b5653e72a96ed4fef04fc3fc
chaitanya <psaichaitanya27@gmail.com>
2025-08-30 13:28:12 +0530

    inital

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import SignUp from './pages/SignUp.jsx'
+import SignIn from './pages/SignIn.jsx'
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/signin' element={<SignIn />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function SignUp() {
+  const primarycolor = '#ff4d2d'
+  const hovercolor = '#e64323'
+  const bgcolor = '#fff9f6'
+  const borderColor = '#ddd'
+
+  return (
+    <div className='min-h-screen w-full flex items-center justify-center' style={{backgroundColor:bgcolor}}>
+      
+
+
+    </div>
+  )
+}
+
+export default SignUp
