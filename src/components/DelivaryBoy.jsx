4e0a4eadc369c4eae009d948c7bd8fbe28f7cbab
chaitanya <psaichaitanya27@gmail.com>
2025-09-21 17:44:40 +0530

    send delivery otp and verify otp

--- src/components/DelivaryBoy.jsx
+++ src/components/DelivaryBoy.jsx
@@ -10,6 +10,7 @@ function DelivaryBoy() {
   const [availableAssignments, setAvailableAssignments] = useState([])
   const [currentOrder, setCurrentOrder] = useState()
   const [showOtpBox,setShowOtpBox] = useState(false)
+  const [otp,setOtp] = useState("")
 
   const getAssignment = async () => {
     try {
@@ -39,9 +40,26 @@ function DelivaryBoy() {
     }
   }
 
-  const handleSendOTP = (e)=>{
-    setShowOtpBox(true)
+  const sendOtp = async () => {
+    
+    try {
+      const result = await axios.post(serverUrl + '/api/order/send-delivery-otp',{orderId:currentOrder._id,shopOrderId:currentOrder.shopOrder._id},{ withCredentials: true })
+      console.log(result.data)
+      setShowOtpBox(true)
+    } catch (error) {
+      console.log(error)
+    }
   }
+
+  const verifyOtp = async () => {
+    try {
+      const result = await axios.post(serverUrl + '/api/order/verify-delivery-otp',{orderId:currentOrder._id,shopOrderId:currentOrder.shopOrder._id,otp},{ withCredentials: true })
+      console.log(result.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
 
   useEffect(() => {
     getAssignment()
@@ -87,11 +105,11 @@ function DelivaryBoy() {
               <p className='text-sm text-gray-400'>{currentOrder.shopOrder.shopOrderItems.length} items | ₹{currentOrder.shopOrder.subtotal}</p>
             </div>
             <DeliveryBoyTracking data={currentOrder} />
-          {!showOtpBox ? <button className='mt-4 w-full bg-green-500 text-white font-semibold py-2 px-4 rounded-xl shadow-md hover:bg-green-600 active:scale-95 transition-all duration-200' onClick={handleSendOTP}>Mark As Delivered</button>  :
+          {!showOtpBox ? <button className='mt-4 w-full bg-green-500 text-white font-semibold py-2 px-4 rounded-xl shadow-md hover:bg-green-600 active:scale-95 transition-all duration-200' onClick={sendOtp}>Mark As Delivered</button>  :
                 <div className='mt-4 p-4 border rounded-xl bg-gray-50'>
                   <p className='text-sm font-semibold mb-2'>Enter Otp send to <span className='text-orange-500 '>{currentOrder.user.fullname}</span></p>
-                  <input type="text" className='w-full border px-3 py-2 rounded-lg mb-3 focus:outline-none focus:ring-2 focus:ring-orange-400' placeholder='Enter OTP' />
-                  <button className='w-full bg-orange-500 text-white py-2 rounded-lg font-semibold hover:bg-orange-600 transition-all '>Submit OTP</button>
+                  <input type="text" className='w-full border px-3 py-2 rounded-lg mb-3 focus:outline-none focus:ring-2 focus:ring-orange-400' placeholder='Enter OTP' value={otp} onChange={(e)=>setOtp(e.target.value)} />
+                  <button className='w-full bg-orange-500 text-white py-2 rounded-lg font-semibold hover:bg-orange-600 transition-all' onClick={verifyOtp} >Submit OTP</button>
                   
                 </div>
           }
